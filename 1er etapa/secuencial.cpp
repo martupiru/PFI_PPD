35a130536918e986589aa17f71d7e0adeb639b18
martupiru <martunahman@gmail.com>
2026-05-14 15:54:47 -0300

    cambios

--- 1er etapa/secuencial.cpp
+++ 1er etapa/secuencial.cpp
@@ -1,40 +1,68 @@
+// parte secuencial del ejercicio de multiplicacion de matrices. C.
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+// Funcion para cargar matrices
+void cargarMatriz(vector<vector<int>>& matriz, string nombre) {
+
+    cout << "\n=== Carga de matriz " << nombre << " ===\n";
+
+    for (int i = 0; i < matriz.size(); i++) {
+        for (int j = 0; j < matriz[i].size(); j++) {
+
+            while (true) {
+                cout << nombre << "[" << i << "][" << j << "] = ";
+
+                if (cin >> matriz[i][j]) {
+                    break; // dato correcto
+                }
+
+                // limpiar error
+                cin.clear();
+                cin.ignore(1000, '\n');
+
+                cout << "ERROR: ingrese un numero valido.\n";
+            }
+        }
+    }
+}
+
+// Funcion para mostrar matrices
+void mostrarMatriz(const vector<vector<int>>& matriz, string nombre) {
+
+    cout << "\nMatriz " << nombre << ":\n";
+
+    for (int i = 0; i < matriz.size(); i++) {
+        for (int j = 0; j < matriz[i].size(); j++) {
+            cout << matriz[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
 
     int M, R, N;
 
-    cout << "Ingrese cantidad de filas de A: ";
+    cout << "Cantidad de filas de A: ";
     cin >> M;
 
-    cout << "Ingrese cantidad de columnas de A / filas de B: ";
+    cout << "Cantidad de columnas de A (y filas de B): ";
     cin >> R;
 
-    cout << "Ingrese cantidad de columnas de B: ";
+    cout << "Cantidad de columnas de B: ";
     cin >> N;
 
     vector<vector<int>> A(M, vector<int>(R));
     vector<vector<int>> B(R, vector<int>(N));
     vector<vector<int>> C(M, vector<int>(N, 0));
 
-    cout << "\n Ingrese matriz A: \n";
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < R; j++) {
-            cin >> A[i][j];
-        }
-    }
+    cargarMatriz(A, "A");
+    cargarMatriz(B, "B");
 
-    cout << "\n Ingrese matriz B: \n";
-    for (int i = 0; i < R; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> B[i][j];
-        }
-    }
-
-    // multiplicacion de matrices
+    // Multiplicacion
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
             for (int k = 0; k < R; k++) {
@@ -43,14 +71,9 @@ int main() {
         }
     }
 
-    cout << "\n Matriz resultado C: \n";
-
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << C[i][j] << " ";
-        }
-        cout << endl;
-    }
+    mostrarMatriz(A, "A");
+    mostrarMatriz(B, "B");
+    mostrarMatriz(C, "Resultado");
 
     return 0;
 }
